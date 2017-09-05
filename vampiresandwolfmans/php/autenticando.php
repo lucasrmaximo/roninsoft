0a04c75a3cb204034f6e706023403e2161a5306a
lucasrmaximo <lucasrmaximo@gmail.com>
2017-09-05 03:19:34 -0300

    v0.0.0.2 criando sistema de autenticação de login

--- vampiresandwolfmans/php/autenticando.php
+++ vampiresandwolfmans/php/autenticando.php
@@ -1,3 +1,28 @@
 <?php
+include "conexao.php";
 
+//session_start();
+
+$usuario = $_POST['edt_usuario'];
+$senha = $_POST['edt_senha'];
+
+echo " $usuario // $senha ";
+
+$sql = "SELECT * FROM `conta` WHERE `usuario` = '$usuario' AND `senha` = '$senha' ";
+
+$linhasconsulta = mysqli_query($conexao,$sql);
+
+if (!$linhasconsulta) {
+   die("erro".mysqli_connect_error());
+}
+
+if($linhasconsulta > 0) {
+
+      //  $_SESSION['usuario']=$_POST['edt_usuario'];
+      //  $_SESSION['senha']=$_POST['edt_senha'];
+
+        echo "Voce foi autenticado com sucesso! Aguarde que logo te redirecionaremos vc.";
+} else {
+        echo " Autenticação falhou, usuario ou senha errados.";
+}
 ?>
--- vampiresandwolfmans/php/conexao.php
+++ vampiresandwolfmans/php/conexao.php
@@ -1,7 +1,11 @@
 <?php
-     $hsot = "localhost";
+     $host = "localhost";
      $user = "root";
      $pass = "";
-     $banco = "cadastro";
-     $conexao = mysql_connect($host, $user, $pass, $banco);
+     $banco = "roninsoft";
+     $conexao = mysqli_connect($host,$user,$pass,$banco);
+     
+     if (!$conexao) {
+        die('Sem conexao, erro de host ou user ou pass:'.mysqli_connect_error());
+     }
 ?>
